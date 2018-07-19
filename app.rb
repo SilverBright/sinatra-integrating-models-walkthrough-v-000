c9f7e19445c06ab402a40d1ff60f909bf1697722
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-19 23:13:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,9 +13,9 @@ class App < Sinatra::Base
   end
 
   post '/' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
-  erb :results
+    erb :results
   end
 
 end
